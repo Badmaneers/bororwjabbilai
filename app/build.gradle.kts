3949d2c2f8c19b4a55e9160d59c090ba104be723
Badmaneers <dukebraham24@gmail.com>
2026-02-05 10:52:40 +0530

    chore: update versionName to 1.2.0 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 2
-        versionName = "1.1.0"
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
