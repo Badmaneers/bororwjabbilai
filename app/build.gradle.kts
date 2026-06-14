ab2d3a8f51718c05426999775e5abc75c5be10ce
Badmaneers <dukebraham24@gmail.com>
2026-06-14 07:52:21 +0530

    Update versionName to 1.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 2
-        versionName = "1.4.4"
+        versionName = "1.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
